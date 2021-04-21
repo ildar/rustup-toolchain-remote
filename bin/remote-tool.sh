ac247420bba2721e4e5b2650b9154462253ae7c2
Ildar Mulyukov <ildar@altlinux.ru>
2021-04-21 11:53:02 +0600

    add protect from unwanted/random file syncs

--- bin/remote-tool.sh
+++ bin/remote-tool.sh
@@ -11,16 +11,21 @@ REMOTE_HOST=${REMOTE_HOST:-localhost}
 REMOTE_SHELL=${REMOTE_SHELL:-ssh}
 REMOTE_SHELL_SHELL_OPT=${REMOTE_SHELL_SHELL_OPT:-}
 SYNC_CWD=${SYNC_CWD:-yes}
+#TODO SYNC_QUIET=${SYNC_QUIET:-yes}
 SYNC_EXCLUDES=${SYNC_EXCLUDES:-target}
 DEST_DIR=${DEST_DIR:-/tmp/remote-builds/`basename "$PWD"`/}
 
-if [ "$SYNC_CWD" = yes -o "$SYNC_CWD" = true ]; then
+[ "$SYNC_CWD" = true ] && SYNC_CWD=yes
+
+if [ "$SYNC_CWD" = yes -a \( -e .git -o -e Cargo.toml \) ]; then
   $REMOTE_SHELL $REMOTE_SHELL_SHELL_OPT -- mkdir -p "$DEST_DIR"
   rsync --rsh="$REMOTE_SHELL" \
     -a --delete --compress --info=progress2 --exclude "$SYNC_EXCLUDES" -- \
     . "$REMOTE_HOST":"$DEST_DIR"
 fi
 
-$REMOTE_SHELL $REMOTE_SHELL_SHELL_OPT -- sh -l -c "cd \"$DEST_DIR\";\"$TOOL_NAME\" $ARGS"
+$REMOTE_SHELL $REMOTE_SHELL_SHELL_OPT -- sh -l -c "cd \"$DEST_DIR\" 2>/dev/null;\"$TOOL_NAME\" $ARGS"
 
 #FIXME add pulling the results
+#FIXME add cleanup option
+
