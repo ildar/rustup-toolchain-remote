4af64dd7cf1e273c703032770d3adb41da1d608e
Ildar Mulyukov <ildar@altlinux.ru>
2021-04-21 11:58:33 +0600

    add SYNC_QUIET option

--- bin/remote-tool.sh
+++ bin/remote-tool.sh
@@ -11,16 +11,18 @@ REMOTE_HOST=${REMOTE_HOST:-localhost}
 REMOTE_SHELL=${REMOTE_SHELL:-ssh}
 REMOTE_SHELL_SHELL_OPT=${REMOTE_SHELL_SHELL_OPT:-}
 SYNC_CWD=${SYNC_CWD:-yes}
-#TODO SYNC_QUIET=${SYNC_QUIET:-yes}
+SYNC_QUIET=${SYNC_QUIET:-no}
 SYNC_EXCLUDES=${SYNC_EXCLUDES:-target}
 DEST_DIR=${DEST_DIR:-/tmp/remote-builds/`basename "$PWD"`/}
 
 [ "$SYNC_CWD" = true ] && SYNC_CWD=yes
+[ "$SYNC_QUIET" = true ] && SYNC_QUIET=yes
+[ "$SYNC_QUIET" = yes ] || RSYNC_PROGRESS="--info=progress2"
 
 if [ "$SYNC_CWD" = yes -a \( -e .git -o -e Cargo.toml \) ]; then
   $REMOTE_SHELL $REMOTE_SHELL_SHELL_OPT -- mkdir -p "$DEST_DIR"
   rsync --rsh="$REMOTE_SHELL" \
-    -a --delete --compress --info=progress2 --exclude "$SYNC_EXCLUDES" -- \
+    -a --delete --compress $RSYNC_PROGRESS --exclude "$SYNC_EXCLUDES" -- \
     . "$REMOTE_HOST":"$DEST_DIR"
 fi
 
