b30ce409ed62badba38afc5e6526afe32c9336ee
Ildar Mulyukov <ildar@altlinux.ru>
2021-04-23 12:37:04 +0600

    fix for ssh usecase

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-This is a "proxy" toolchain for using a remote installation of Rust/Cargo etc.
+This is a "proxy" toolchain for using of a remote installation of Rust/Cargo etc.
 from the "light" local PC installation.
 
-== Installation
+## Installation
 
 1. Start as in the manual:
 > $ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 
 2. copy this whole project folder to the `toolchains/` folder:
-> $ mkdir -p ~/.rustup/toolchains
-> $ cp -a ../rustup-toolchain-remote ~/.rustup/toolchains/
+> $ mkdir -p ~/.rustup/toolchains ; cp -a ../rustup-toolchain-remote ~/.rustup/toolchains/
 
 3. make it default
 > $ rustup default rustup-toolchain-remote
 
 4. test it!
-> rustc -vV
+> $ rustc -vV
 
--- bin/remote-tool.sh
+++ bin/remote-tool.sh
@@ -9,7 +9,8 @@ ARGS="$*"
 
 REMOTE_HOST=${REMOTE_HOST:-localhost}
 REMOTE_SHELL=${REMOTE_SHELL:-ssh}
-REMOTE_SHELL_SHELL_OPT=${REMOTE_SHELL_SHELL_OPT:-}
+REMOTE_SHELL_OPT=${REMOTE_SHELL_OPT:-}
+REMOTE_SHELL_SHELL_WRAP=${REMOTE_SHELL_SHELL_WRAP:-}
 SYNC_CWD=${SYNC_CWD:-yes}
 SYNC_QUIET=${SYNC_QUIET:-no}
 SYNC_EXCLUDES=${SYNC_EXCLUDES:-target}
@@ -20,13 +21,13 @@ DEST_DIR=${DEST_DIR:-/tmp/remote-builds/`basename "$PWD"`/}
 [ "$SYNC_QUIET" = yes ] || RSYNC_PROGRESS="--info=progress2"
 
 if [ "$SYNC_CWD" = yes -a \( -e .git -o -e Cargo.toml \) ]; then
-  $REMOTE_SHELL $REMOTE_SHELL_SHELL_OPT -- mkdir -p "$DEST_DIR"
+  $REMOTE_SHELL $REMOTE_SHELL_OPT "$REMOTE_HOST" -- mkdir -p "$DEST_DIR"
   rsync --rsh="$REMOTE_SHELL" \
     -a --delete --compress $RSYNC_PROGRESS --exclude "$SYNC_EXCLUDES" -- \
     . "$REMOTE_HOST":"$DEST_DIR"
 fi
 
-$REMOTE_SHELL $REMOTE_SHELL_SHELL_OPT -- sh -l -c "cd \"$DEST_DIR\" 2>/dev/null;\"$TOOL_NAME\" $ARGS"
+$REMOTE_SHELL $REMOTE_SHELL_OPT "$REMOTE_HOST" -- $REMOTE_SHELL_SHELL_WRAP "cd \"$DEST_DIR\" 2>/dev/null;\"$TOOL_NAME\" $ARGS"
 
 #FIXME add pulling the results
 #FIXME add cleanup option
