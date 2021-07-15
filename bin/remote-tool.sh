1b76005bd01b4cc50cb58f99c08dcf28fd07565e
Ildar Mulyukov <ildar@altlinux.ru>
2021-07-15 11:09:25 +0600

    REMOTE_SHELL_STDIO_TRANS option
    to translate local to remote WD and vice versa
    FIXME: forward translation doesn't work yet

--- bin/remote-tool.sh
+++ bin/remote-tool.sh
@@ -11,6 +11,7 @@ REMOTE_HOST=${REMOTE_HOST:-localhost}
 REMOTE_SHELL=${REMOTE_SHELL:-ssh}
 REMOTE_SHELL_OPT=${REMOTE_SHELL_OPT:-}
 REMOTE_SHELL_SHELL_WRAP=${REMOTE_SHELL_SHELL_WRAP:-}
+REMOTE_SHELL_STDIO_TRANS=${REMOTE_SHELL_STDIO_TRANS:-yes}
 SYNC_CWD=${SYNC_CWD:-yes}
 SYNC_QUIET=${SYNC_QUIET:-yes}
 SYNC_EXCLUDES=${SYNC_EXCLUDES:-target}
@@ -32,6 +33,14 @@ else
   unset DEST_DIR
 fi
 
+# FIXME: FW doesn't work yet
+STDIO_TRANS_FW=cat
+STDIO_TRANS_RE=cat
+[ "$REMOTE_SHELL_STDIO_TRANS" = true ] && REMOTE_SHELL_STDIO_TRANS=yes
+[ "$REMOTE_SHELL_STDIO_TRANS" = yes ] && \
+  STDIO_TRANS_FW="sed s|$SYNC_SOURCE|$DEST_DIR|g" &&
+  STDIO_TRANS_RE="sed s|$DEST_DIR|$SYNC_SOURCE|g"
+
 if [ "$SYNC_CWD" = yes ] && [ -n "$DEST_DIR" ]; then
   $REMOTE_SHELL $REMOTE_SHELL_OPT "$REMOTE_HOST" -- mkdir -p "$DEST_DIR"
   rsync --rsh="$REMOTE_SHELL" \
@@ -39,7 +48,11 @@ if [ "$SYNC_CWD" = yes ] && [ -n "$DEST_DIR" ]; then
     "$SYNC_SOURCE"/. "$REMOTE_HOST":"$DEST_DIR"/
 fi
 
-$REMOTE_SHELL $REMOTE_SHELL_OPT "$REMOTE_HOST" -- $REMOTE_SHELL_SHELL_WRAP ". \$HOME/.cargo/env; cd \"$DEST_DIR\" 2>/dev/null;\"$TOOL_NAME\" $ARGS"
+# FIXME: FW doesn't work yet
+#perl -MFcntl -e 'fcntl STDIN, F_SETFL, fcntl(STDIN, F_GETFL, 0) | O_NONBLOCK'
+#$STDIO_TRANS_FW | \
+$REMOTE_SHELL $REMOTE_SHELL_OPT "$REMOTE_HOST" -- $REMOTE_SHELL_SHELL_WRAP ". \$HOME/.cargo/env; cd \"$DEST_DIR\" 2>/dev/null;\"$TOOL_NAME\" $ARGS" | \
+$STDIO_TRANS_RE
 
 #FIXME add pulling the results
 #FIXME add cleanup option
